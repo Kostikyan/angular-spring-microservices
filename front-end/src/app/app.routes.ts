7d0d9811685347ab7eea2cf235b13bac0b43967c
Vahe <120592399+Kostikyan@users.noreply.github.com>
2023-11-27 00:18:46 +0400

    Home Page 🏠

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,10 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {RegisterComponent} from "./pages/register/register.component";
 import {Error404} from "./pages/404/404.component";
+import {HomeComponent} from "./pages/home/home.component";
 
 export const routes: Routes = [
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "**", component: Error404 },
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "home", component: HomeComponent},
+
+  {path: "**", component: Error404},
 ];
